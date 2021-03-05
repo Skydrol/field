a7187362637ebd0ed886f171807d838375956213
skydrol <keineleben@gmail.com>
2021-03-04 21:36:36 -0300

    code finished

--- command.js
+++ command.js
@@ -1,11 +1,12 @@
 const program = require('commander');
 const {
     getNumberOfCarsByBrand, //and the --list
-    getNumberOfCarsByMileage, //and the --list
+    getNumberOfCarsByMileages, //and the --list
     getTotalValueByDealership
 
 } = require('./main');
 
+//==================== CARS BY BRAND ======================
 program
     .command('getNumberOfCarsByBrand <brand>')
     .alias('cars-by-brand')
@@ -14,15 +15,28 @@ program
     .action((brand, options) => {
         getNumberOfCarsByBrand(brand, options.list);
     });
+//=========================================================
 
+//==================== CARS BY DEALER =====================
 program
     .command('getTotalValueByDealership <dealership>')
-    .alias('cars-by-dealer')
+    .alias('total-value-by-dealer')
     .description('get the total value of cars that exist in a given dealership')
-    .option('-l, --list', 'list cars')
     .action((dealership) => {
         getTotalValueByDealership(dealership);
     });
+//=========================================================
+
+//==================== CARS BY MILEAGE ====================
+program
+    .command('getNumberOfCarsByMileages <minMileage>, <maxMileage>')
+    .alias('cars-by-mileage-range')
+    .description('get the number of cars and the list of cars by mileage range (parameters: maxMileage, minMileage)')
+    .action((minMileage, maxMileage) => {
+        getNumberOfCarsByMileages(minMileage, maxMileage);
+    });
+//=========================================================
+//getNumberOfCarsByMileages
 
 
 program.parse(process.argv);
--- main.js
+++ main.js
@@ -1,56 +1,87 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const filePath = 'backend_file-calculation_dealership.csv';
 
 const getNumberOfCarsByBrand = (brand, list) => {
-    let results = [];
-    fs.createReadStream('backend_file-calculation_dealership.csv')
-        .pipe(csv(['brand', 'dealer', 'mileage', 'price']))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            var numberOfcars = 0;
-            results.forEach(element => {
-                if (element.brand.toLowerCase() == brand.toLowerCase()) {
-                    numberOfcars++;
-                    if (list) {
-                        console.log(element);
-                    }
+    var results = [];
+    (async() => {
+        results = await csvParse();
+        var numberOfcars = 0;
+        results.forEach(car => {
+            if (car.brand.toLowerCase() == brand.toLowerCase()) {
+                numberOfcars++;
+                if (list) {
+                    console.log(car);
                 }
-            });
-            console.log('\nTotal of cars with brand name "' + brand + '" : ' + numberOfcars);
+            }
         });
-
-
+        console.log('\nTotal of cars with brand name "' + brand + '" : ' + numberOfcars);
+    })();
 }
 
-const getNumberOfCarsByMileage = (maxMileage, minMileage) => {
+const getNumberOfCarsByMileages = (minMileage, maxMileage) => {
+    var results = [];
+    (async() => {
+        results = await csvParse();
+        var numberOfCars = 0;
+        results.forEach(car => {
+            var carMileage = parseFloat(car.mileage);
+            if (carMileage > parseFloat(minMileage) && carMileage < parseFloat(maxMileage)) {
+                console.log(car);
+                numberOfCars++;
+            }
 
+        });
+        console.log('\nTotal of cars in mileages range: ' + numberOfCars);
+    })();
 }
 
 const getTotalValueByDealership = (dealership) => {
-    let results = [];
-    fs.createReadStream('backend_file-calculation_dealership.csv')
-        .pipe(csv(['brand', 'dealer', 'mileage', 'price']))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            var totalValue = 0;
-            results.forEach(element => {
-                if (element.dealer.toLowerCase() == dealership.toLowerCase()) {
-                    totalValue = totalValue + parseInt(element.price);
-
-                }
-            });
-            console.log('\nTotal value of cars of dealership "' + dealership + '" : ' + totalValue);
+    var results = [];
+    (async() => {
+        results = await csvParse();
+        var totalValue = 0;
+        results.forEach(car => {
+            if (car.dealer.toLowerCase() == dealership.toLowerCase()) {
+                totalValue = totalValue + parseInt(car.price);
+            }
         });
+        console.log('\nTotal value of cars of dealership "' + dealership + '" : EUR ' + formatMoney(totalValue));
+    })();
 }
+
+function csvParse() {
+    let array = [];
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filePath)
+            .pipe(csv(['brand', 'dealer', 'mileage', 'price']))
+            .on("error", error => reject(error))
+            .on("data", data => array.push(data))
+            .on("end", () => {
+                resolve(array);
+            });
+    });
+
+}
+
+function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+    try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+        const negativeSign = amount < 0 ? "-" : "";
+
+        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        let j = (i.length > 3) ? i.length % 3 : 0;
+
+        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+        console.log(e)
+    }
+};
 
 module.exports = {
     getNumberOfCarsByBrand,
     getTotalValueByDealership,
-    getNumberOfCarsByMileage
+    getNumberOfCarsByMileages
 }
-
-
-
-//and the --list
-//, //and the --list
-//totalValueByDealership
